36fa2ccf816981a7e3fe625b974397ca670189a3
Khanh Hoang <hoangchaukhanh93@gmail.com>
2019-02-24 16:35:15 +0700

    Remove unused index.css file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import ErrorBoundary from './bugsnag'
-import './index.css'
 import * as serviceWorker from './serviceWorker'
 import createConfigStore from './store'
 
