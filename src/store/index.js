110d3fc8ff57fdffacbbcc76e6d2911f5ade9b2d
Khanh Hoang <hoangchaukhanh93@gmail.com>
2019-02-24 17:39:58 +0700

    Integrate LogRocket for Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import LogRocket from 'logrocket'
 import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers'
@@ -5,5 +6,8 @@ import reducers from './reducers'
 export default function createConfigStore() {
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-  return createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+  return createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(thunk, LogRocket.reduxMiddleware()))
+  )
 }
